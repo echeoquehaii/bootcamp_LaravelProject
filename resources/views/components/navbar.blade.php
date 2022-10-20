9e01e0ef072cd71add4a3d88fa2c2c775607b53f
Giorgia Angiolini <giorgiaangiolini.info@gmail.com>
2022-10-20 16:53:59 +0200

    done homepage

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,24 @@
+{{-- navbar --}}
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <i class="fa-solid fa-glasses"></i>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Servizi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contatti</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Chi siamo</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+<x-overall>
+<x-navbar> 
+</x-navbar>
+
+ <div class="hero">
+  <h1>Sito di occhiali belli</h1>
+ </div>
+
+
+
+  
+
+</x-overall>
